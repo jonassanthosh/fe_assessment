5c93c59569f2639ed1a1e34e9075d360e2ead31b
jonassanthosh <jonas13ts@gmail.com>
2023-03-01 14:29:46 +0530

    card body ui , avatar link, like button  - done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import { useEffect, useState } from 'react';
 import UserCard from './components/UserCard'
-import { Col, Row, Space } from 'antd';
+import { Col, Row} from 'antd';
 
 function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <Row>
-      <Col xl={6} lg={8} md={8} sm={24} xs={24}  ><UserCard /></Col>
-      <Col xl={6} lg={8} md={8} sm={24} xs={24}  ><UserCard /></Col>
-      <Col xl={6} lg={8} md={8} sm={24} xs={24}  ><UserCard /></Col>
-      <Col xl={6} lg={8} md={8} sm={24} xs={24}  ><UserCard /></Col>
-      <Col xl={6} lg={8} md={8} sm={24} xs={24}  ><UserCard /></Col>
-      <Col xl={6} lg={8} md={8} sm={24} xs={24}  ><UserCard /></Col>
-      <Col xl={6} lg={8} md={8} sm={24} xs={24}  ><UserCard /></Col>
-      <Col xl={6} lg={8} md={8} sm={24} xs={24}  ><UserCard /></Col>
-      
-      
+      {users.map((user, index) => {
+        return (
+          <Col xl={6} lg={8} md={8} sm={24} xs={24} key={index}><UserCard user={user} index={index} /></Col>
+        )
+      })}
     </Row>
   );
 }
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,28 +1,34 @@
-import React from 'react'
-import { EditOutlined, DeleteFilled, HeartOutlined, MailOutlined } from '@ant-design/icons';
-import { Avatar, Card } from 'antd';
-const { Meta } = Card;
+import React, { useState } from 'react'
+import { EditOutlined, DeleteFilled, HeartOutlined, MailOutlined, PhoneOutlined, GlobalOutlined, HeartFilled } from '@ant-design/icons';
+import { Card } from 'antd';
 
-const UserCard = () => (
-    <Card
-        style={{
-            margin: 15
-        }}
-        cover={
-            <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-            />
-        }
-        actions={[
-            <HeartOutlined />,
-            <EditOutlined key="edit" />,
-            <DeleteFilled />
-        ]}
-    >
-        <h3>Leanne Graham</h3>
-        
-    </Card>
-)
+const UserCard = ({ user, index }) => {
+    const [like, setLike] = useState(false)
+    return (
+        <Card
+            style={{
+                margin: 15
+            }}
+            cover={
+                <img
+                    alt="avatar"
+                    src={`https://avatars.dicebear.com/v2/avataaars/${user.username}.svg?options[mood][]=happy`}
+                    width={200}
+                    height={200}
+                />
+            }
+            actions={[
+                like ? <HeartFilled onClick={() => setLike((prev) => !prev)} /> : <HeartOutlined onClick={() => setLike((prev) => !prev)} />,
+                <EditOutlined key="edit" />,
+                <DeleteFilled />
+            ]}
+        >
+            <h3 style={{ margin: '0', paddingBottom: '8px', fontWeight: '500' }}>{user.name}</h3>
+            <div style={{ display: 'flex' }}><MailOutlined style={{ fontSize: '18px' }} /><p style={{ paddingLeft: '10px', paddingBottom: '5px', margin: '0' }}>{user.email}</p></div>
+            <div style={{ display: 'flex' }}><PhoneOutlined style={{ fontSize: '18px' }} /><p style={{ paddingLeft: '10px', paddingBottom: '5px', margin: '0' }}>{user.phone}</p></div>
+            <div style={{ display: 'flex' }}><GlobalOutlined style={{ fontSize: '18px' }} /><p style={{ paddingLeft: '10px', paddingBottom: '5px', margin: '0' }}>http://{user.website}</p></div>
+        </Card>
+    )
+}
 
 export default UserCard
